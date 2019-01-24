0d82c3cdb4177f0a97086d815955fd578270b38e
Atlas Chan <cnchanac@connect.ust.hk>
2019-01-24 12:11:03 +0800

    Added Pdf display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Keys from './DisplayKeys';
 import pdfFile from './sample.pdf';
 import PDF from 'react-pdf-js';
+import utf8 from 'utf8';
+import base64 from 'js-base64';
 
 class App extends Component {
   state = {
@@ -12,25 +14,38 @@ class App extends Component {
   }
 
   updateState = (jsonObject) =>{
-    let str = jsonObject["pdfinfo"];
-    //console.log(this.base64Decode(str))
+    let encodedBytes = jsonObject["pdfinfo"];
+    //console.log(this.base64Decode(str)
+    var base64str = encodedBytes;
 
+    // decode base64 string, remove space for IE compatibility
+    var binary = atob(base64str.replace(/\s/g, ''));
+    var len = binary.length;
+    var buffer = new ArrayBuffer(len);
+    var view = new Uint8Array(buffer);
+    for (var i = 0; i < len; i++) {
+        view[i] = binary.charCodeAt(i);
+    }
 
-
+    // create the blob object with content-type "application/pdf"               
+    var blob = new Blob( [view], { type: "application/pdf" });
+    var url = URL.createObjectURL(blob);
+    
     this.setState({
       jsonObject:jsonObject,
-      extractedPdf:str
+      extractedPdf:url
     })
   }
 
   render() {
+    console.log(this.state.extractedPdf)
     return (
     <div>
       <h1>HKUST Blockcert Verifier</h1>
       <DropBar updateJson={this.updateState}/>
       <br/>
       <Keys/>
-      <PDF file={this.state.extractedPdf || pdfFile}/>
+      {this.state.extractedPdf && <PDF file={this.state.extractedPdf}/>}
     </div>);
   }
 }
