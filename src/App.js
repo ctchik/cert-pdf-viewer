bef1639bccb4b7d6cdb545bc498e5e139e969532
Atlas Chan <cnchanac@connect.ust.hk>
2019-06-06 02:11:09 +0800

    html5 integration

--- src/App.js
+++ src/App.js
@@ -3,12 +3,37 @@ import DropBar from './DropBar';
 import './App.css';
 import Keys from './DisplayKeys';
 import Document from './Document';
+import Error from './Error';
 
 class App extends Component {
   state = {
     jsonObject:null,
-    extractedPdf:null,
-    keys:null
+    extractedContent:null,
+    fileType:null,
+    keys:null,
+    verification:null,
+    error:null
+  }
+
+  setError = (error) => {
+    this.setState({
+      error:error
+    })
+  }
+
+  clearState = () => {
+    this.setState({
+      extractedContent:null,
+      verification:null,
+      error:null,
+      fileType:null
+    })
+  }
+
+  updateStatus = report => {
+    this.setState({
+      verification:report
+    })
   }
 
   updateKeys = keys => {
@@ -17,45 +42,56 @@ class App extends Component {
     })
   }
 
-  updateState = (jsonObject) =>{
-    let encodedBytes = jsonObject["pdfinfo"];
-    //console.log(this.base64Decode(str)
-    var base64str = encodedBytes;
+  updateState = (jsonObject, fileType) =>{
+    try{
+        let content = jsonObject['filecontent'];
+        let encodedBytes = content;
+        //console.log(this.base64Decode(str)
+        var base64str = encodedBytes;
 
-    // decode base64 string, remove space for IE compatibility
-    var binary = atob(base64str.replace(/\s/g, ''));
-    var len = binary.length;
-    var buffer = new ArrayBuffer(len);
-    var view = new Uint8Array(buffer);
-    for (var i = 0; i < len; i++) {
-        view[i] = binary.charCodeAt(i);
+        // decode base64 string, remove space for IE compatibility
+        var binary = atob(base64str.replace(/\s/g, ''));
+        var len = binary.length;
+        var buffer = new ArrayBuffer(len);
+        var view = new Uint8Array(buffer);
+        for (var i = 0; i < len; i++) {
+            view[i] = binary.charCodeAt(i);
+        }
+
+        // create the blob object with content-type "application/pdf"               
+        var blob = new Blob( [view], { type: "application/pdf" });
+        var url = URL.createObjectURL(blob);
+        this.setState({
+          jsonObject:jsonObject,
+          extractedContent:url,
+          fileType: fileType
+        })
+        console.log(url);
+    }catch(e){
+      if(this.state.error == null){
+        this.setError("Unexpected Error: Failed to extract Content.");
+      }
+      return;
     }
-
-    // create the blob object with content-type "application/pdf"               
-    var blob = new Blob( [view], { type: "application/pdf" });
-    var url = URL.createObjectURL(blob);
-
-    this.setState({
-      jsonObject:jsonObject,
-      extractedPdf:url
-    })
   }
 
   render() {
     return (
-    <div>
-      <h1>HKUST Blockcert Verifier</h1>
-      <DropBar keys={this.state.keys} updateJson={this.updateState} setClick={postFunction => this.handleClick = postFunction}/>
+    <div className="container">
+      <h1 className="header">HKUST Blockcerts Verifier</h1>
+      <DropBar contentUrl={this.state.extractedContent} setError={this.setError} clearState={this.clearState} updateStatus={this.updateStatus} keys={this.state.keys} updateJson={this.updateState} setClick={postFunction => this.handleClick = postFunction}/>
       <br/>
       <Keys updateKeys={this.updateKeys}/>
-      {this.state.extractedPdf && 
+      {this.state.error && <div className="text-danger">{this.state.error}</div>}
+      <Error verification={this.state.verification}/>
+      {this.state.fileType==="pdf" && this.state.verification && (this.state.verification[6].passed) && this.state.extractedContent && 
       <div>
-        <a href={this.state.extractedPdf} download="certificate.pdf">
+        <a href={this.state.extractedContent} download="certificate.pdf">
           <button type="button" className="btn btn-primary" style={{marginRight:30}}>Download PDF</button>
         </a>
         <button type="button" className="btn btn-primary" onClick={() => this.handleClick(this.state.jsonObject)}>Verify Again!</button>
         <div>
-          <Document pdf={this.state.extractedPdf}/>
+          <Document pdf={this.state.extractedContent}/>
         </div>
       </div>}
     </div>);
--- src/DisplayKeys.js
+++ src/DisplayKeys.js
@@ -2,6 +2,9 @@ import React from 'react';
 import axios from 'axios'
 
 class Keys extends React.Component{
+    /**
+     * Fetching keys from the HKUST's server.
+     */
     constructor(props){
         super(props);
         this.state = {
@@ -9,14 +12,20 @@ class Keys extends React.Component{
         }
     }
     componentDidMount(){
-        axios.get('http://143.89.2.220:5000/keys').then((res)=>{
-            this.props.updateKeys(res.data)
-            this.setState({
-                keys:res.data
+        try{
+            axios.get('http://127.0.0.1:5000/keys').then((res)=>{
+                this.props.updateKeys(res.data)
+                this.setState({
+                    keys:res.data
+                })
             })
-        })
+        }
+        catch(e){
+            console.log("Not able to access server.");
+            return;
+        }   
     }
-    //keys will store an array of keys
+    //keys store an array of keys
     render(){
         if(this.state.keys == null){
             return <div><b>Error, no key is fetched from server.</b></div>
